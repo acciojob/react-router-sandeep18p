1525d273b5609fe5e1ce99a0802a50fd7c4f957f
sandeep18p <sandeep.pansari98@gmail.com>
2024-05-14 17:08:45 +0530

    one

---
+++ src/components/About.js
@@ -0,0 +1,13 @@
+// About.js
+import React from 'react';
+
+const About = () => {
+  return (
+    <div>
+        <h1>About</h1>
+     This is a simple React Router Program.
+    </div>
+  );
+};
+
+export default About;
--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,30 @@
-
-import React from "react";
-import './../styles/App.css';
+// App.js
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import Home from './Home';
+import About from './About';
 
 const App = () => {
   return (
-    <div>
-        {/* Do not remove the main div */}
-    </div>
-  )
-}
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+          </ul>
+        </nav>
+        <Routes>
+          <Route path="/about" element={<About />} />
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </div>
+    </Router>
+  );
+};
 
-export default App
+export default App;
